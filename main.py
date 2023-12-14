2a634f4b0f61f97db0d1bb88755dde707a5cb49d
Odium <LucaPirnke@aol.com>
2023-12-14 12:47:13 +0100

    MySQL und App Fenster

--- main.py
+++ main.py
@@ -5,21 +5,18 @@ customtkinter.set_appearance_mode("System")
 customtkinter.set_default_color_theme("blue")
 
 app = customtkinter.CTk()
-app.title("Das Dokument|Login")
 app.geometry("600x500")
 app.resizable(False, False)
+app.title("Das Dokument | Login")
 
-app.label = customtkinter.CTkLabel(app, text="Login", font=("Arial", 20))
-app.label.pack(pady=20)
+# MySQL Connection
+try:
+    connection = mysql.connector.connect(host="62.171.164.75", database="DasDokument", user="system", password="BZwpCdIKsKM4MREYV8Bi")
+    if connection.is_connected():
+        db_Info = connection.get_server_info()
+        print("Connected to MySQL Server version ", db_Info)
+        cursor = connection.cursor()
+except Error as e:
+    print("Error while connecting to MySQL", e)
 
-app.textlabel = customtkinter.CTkLabel(app, text="Username", font=("Arial", 20))
-app.textlabel.pack(pady=20)
-app.input = customtkinter.CTkEntry(app, width=20)
-app.input.pack(pady=20)
-
-app.textlabel2 = customtkinter.CTkLabel(app, text="Password", font=("Arial", 20))
-app.textlabel2.pack(pady=20)
-app.input2 = customtkinter.CTkEntry(app, width=20)
-app.input2.pack(pady=20)
-
-app.mainloop
+app.mainloop()
